e93bc54bfd559c70882b9503d9a416bbe07b79a8
Ebishu-0309 <72.ebihara.shusaku@tsukukoma-gafe.org>
2020-05-23 11:52:11 +0900

    fix

--- js/header.js
+++ js/header.js
@@ -1,17 +1,3 @@
-/*
- <h1>Ebishu's Sparebeat Page</h1>
-        <div class="header">
-            <div class="header-list">
-                <ul>
-                    <li><a href="index.html">ƒz[ƒ€</a></li>
-                    <li><a href="scores.html">•ˆ–Êˆê——</a></li>
-                    <li><a href="contact.html">contact</a></li>
-                    <li><a href="https://sparebeat.com/settings"target="_blank" rel="noopener noreferrer">İ’è</a> </li>
-                </ul>
-            </div>
-        </div>
-        */
-
 var h1, div1, div2, ul, li, a;
 h1 = document.createElement('h1'), div1 = document.createElement('div'), div2 = document.createElement('div'),
     ul = document.createElement('ul');
@@ -21,6 +7,9 @@ var url = ["scores.html", "update.html", "contact.html", "https://sparebeat.com/
 for (var i = 0; i < 4; ++i) {
     var a = document.createElement('a'), li = document.createElement('li');
     a.href = url[i], a.textContent = text[i];
+    if (i == 3) {
+        a.target = "_blank", a.rel = "noopener noreferrer";
+    }
     li.appendChild(a);
     ul.appendChild(li);
 }
